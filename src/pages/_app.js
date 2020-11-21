6abd056caa5125fa6a905707379f11312ac09470
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-11-21 15:30:04 +0800

    Add Firebase SDKs and initialize Firebase

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from 'firebase/app';
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import 'firebase/analytics';
+// Add the Firebase products that you want to use
+import 'firebase/auth';
+import 'firebase/firestore';
+import firebaseConfig from '../constants/firebase-config.json';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-export default MyApp
+if (typeof window !== 'undefined') {
+  firebase.analytics();
+}
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
