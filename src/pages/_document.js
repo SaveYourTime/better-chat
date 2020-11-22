731e8fd66074be55ee4d436af987ade11e3dd686
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-11-23 02:03:09 +0800

    Remove useless external script files

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,16 +16,6 @@ class MyDocument extends Document {
             integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ=="
             crossOrigin="anonymous"
           />
-          <script
-            src="https://code.jquery.com/jquery-3.1.1.min.js"
-            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-            crossOrigin="anonymous"
-          />
-          <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"
-            integrity="sha512-dqw6X88iGgZlTsONxZK9ePmJEFrmHwpuMrsUChjAw1mRUhUITE5QU9pkcSox+ynfLhL15Sv2al5A0LVyDCmtUw=="
-            crossOrigin="anonymous"
-          />
         </Head>
         <body>
           <Main />
